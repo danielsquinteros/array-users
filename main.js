e6d079ea0d5719eaa2cc1f93b5cfff34833ec92c
Daniel Quinteros <quinterosm.daniel@gmail.com>
2023-05-10 21:33:47 -0400

    user: repo

--- main.js
+++ main.js
@@ -5,47 +5,34 @@ console.log('HOLA MUNDO')
 const root = document.getElementById('root')
 
 // Paso 5 - Crear función que insertará HTML en el root
-const renderHTML = (urlImage, title, date) => {
+const renderHTML = (name, email, websiteURL) => {
     return `
-    <div class="card mb-3 mt-4">
-        <img src=${urlImage} class="card-img-top image-nasa" alt="...">
+    <div class="card mt-2 mb-2">
+        <h5 class="card-header">Usuario</h5>
         <div class="card-body">
-        <h5 class="card-title">${title}</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text"><small class="text-body-secondary">${date}</small></p>
+        <h5 class="card-title">${name}</h5>
+        <p class="card-text">${email}</p>
+        <a target="_blank" href="https://${websiteURL}" class="btn btn-primary">Go somewhere</a>
         </div>
     </div>
     `
 }
 
-// Paso 1 - Obtener la información que ingrese en el input latitud
-const inputLatitud = document.getElementById('inputLatitud')
-
-//Paso 2 - Obtener la información que ingrese en el input longitud
-const inputLongitud = document.getElementById('inputLongitud')
-
 //Paso 3 - Necesito cuando la persona haga click
 const buttonGetImageNasa = document.getElementById('buttonGetImageNasa')
 
 buttonGetImageNasa.addEventListener('click', async () => {
-    const lat = inputLatitud.value;
-    const lon = inputLongitud.value;
-
-    //Verificación de valores
-    if(!lat || !lon){
-        alert('Falta ingresar latitud o longitud, verifica tu formulario')
-        return
-    }
-
     // then y catch
     try {
-        const response = await fetch(`https://api.nasa.gov/planetary/earth/assets?lon=${lon}&lat=${lat}&date=2021-01-10&dim=0.10&api_key=rMxkO9eCV5NlwONJbvLG1CH39CcSCEJJmgrK42GS`, {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/users`, {
             method: "GET"
         })
     
-        const responseNasa = await response.json();
-        
-        root.innerHTML = renderHTML(responseNasa.url,responseNasa.resource.planet, responseNasa.date )
+        const users = await response.json();
+        const renderUsers = users.map(user => {
+            return renderHTML(user.name, user.email, user.website)
+        })
+        root.innerHTML = renderUsers.join(" ");
         
     } catch (error) {
         root.innerHTML = `<div class="mt-3 alert alert-danger" role="alert">
